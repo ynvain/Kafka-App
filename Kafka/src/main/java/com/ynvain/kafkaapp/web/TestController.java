8431d141f443e99e5b90601f009b25257394905e
Ivan Grigoryev <igrigoryev@pmsoft.ru>
2025-12-25 22:50:52 +0300

    Add basic proj structure and config for kafka

---
+++ Kafka/src/main/java/com/ynvain/kafkaapp/web/TestController.java
@@ -0,0 +1,15 @@
+package com.ynvain.kafkaapp.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+  @GetMapping
+  public String get() {
+    return "Hey you";
+  }
+}
